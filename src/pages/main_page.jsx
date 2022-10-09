3789473861a0122a1804d9c6b5e52469ef5c6d66
문재웅 <63780449+icecrao2@users.noreply.github.com>
2022-10-09 07:26:11 +0000

    상세페이지 제작 완료

--- src/pages/main_page.jsx
+++ src/pages/main_page.jsx
@@ -21,7 +21,6 @@ export const MainPage = ({ user }) => {
   return (
     <div style={{
       width: '100%',
-      height: '100%'
     }}>
       < Routes >
         <Route path='now_playing'
--- src/pages/main_page_mains/detail.jsx
+++ src/pages/main_page_mains/detail.jsx
@@ -1,21 +1,24 @@
 import { useEffect } from 'react';
-import styles from '../../styles/now_playing/now_playing.module.css';
+import styles from '../../styles/detail/detail.module.css';
 
-import { faStar } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { useParams } from "react-router-dom";
 import { getDetailMovie } from '../../api/get_movie.js';
 import { movie_detail } from '../../hooks/movie_detail.js';
+import screenSizeManager from '../../hooks/check_screen_size.js';
 
 export const Detail = ({ dibed_list }) => {
 
-  const { dibedList, handleDibedList } = dibed_list;
+  const {width: screenWidth} = screenSizeManager();
   const { movieDetail, handlemovieDetail } = movie_detail();
 
   const { id } = useParams();
 
-  useEffect(
+  const url = screenWidth > 700 ? 
+    'https://image.tmdb.org/t/p/w500/' : 'https://image.tmdb.org/t/p/w400/'
+
+  
+  useEffect (
     () => {
       async function getMovie() {
         const movie = await getDetailMovie(id);
@@ -26,11 +29,83 @@ export const Detail = ({ dibed_list }) => {
     []
   );
 
+  const hrStyle={
+    background:'#959595',
+    height:'1px',
+    border:'0',
+  }
+
   console.log(movieDetail);
 
   return (
-    <div>
-      <h1>{movieDetail.title}</h1>
+    <div className={styles.main}>
+      
+      <h1 className={styles.title__main}>영화 상세</h1>
+      <hr style={hrStyle}/>
+
+      
+      
+      <div className={styles.everyDivision}>
+        
+        <div>
+          <img
+                  src={`${url}${movieDetail.poster_path}`}></img>
+        </div>
+        <div className={styles.summaryDivision}>
+          
+          <h1 className={styles.title}>{movieDetail.title} 
+            {
+              movieDetail.status ==='Released' ? 
+              <span className={styles.status}>(상영중)</span>
+              : 
+              <span className={styles.status}>(상영예정)</span>
+            }
+          
+          </h1>
+          
+          <hr style={hrStyle}/>
+          
+          <div className={styles.companiesContainer}>
+            <span className={styles.weight}>{movieDetail.tagline}</span>
+          </div>
+          
+          <hr style={hrStyle}/>
+          
+          <div className={styles.overviewContainer}>
+            <h2 className={styles.secondTitle}>줄거리</h2>
+            <p className={styles.overview}>{movieDetail.overview}</p>
+          </div>
+
+          <hr style={hrStyle}/>
+  
+          <div className={styles.releaseContainer}>
+            <h2 className={styles.secondTitle}>개봉일</h2>
+            <span className={styles.weight}>{movieDetail.release_date}</span>
+          </div>
+
+          <hr style={hrStyle}/>
+  
+          <div className={styles.companiesContainer}>
+            <h2 className={styles.secondTitle}>평점</h2>
+            <span className={styles.weight}>{movieDetail.vote_average}</span>
+          </div>
+
+          <hr style={hrStyle}/>
+  
+          <div className={styles.companiesContainer}>
+            <h2 className={styles.secondTitle}>제작사</h2>
+            {
+              movieDetail.production_companies ?
+              movieDetail.production_companies.map((item) => <span className={styles.companies}>&nbsp;&nbsp;&nbsp;||&nbsp;&nbsp;&nbsp;${item.name}</span> )
+              :
+              <span>none</span>
+            }
+          </div>
+
+          <hr style={hrStyle}/>
+          
+        </div>
+      </div>
     </div>
   );
 }
